02cda684471c43c1f5eb770d261a2873ba0b8a3d
Yukult <gula421@gmail.com>
2021-05-18 16:36:28 +0200

    add functions to sort and filter table

---
+++ sortTable.js
@@ -0,0 +1,75 @@
+function filterKeywords(){
+  var word = document.getElementById("keywordFilter").value;
+  var table = document.getElementsByClassName("overview_table");
+  var rows = table[0].rows;
+  for (var i=1; i<rows.length; i++){ // differernt rows
+      var tds = rows[i].getElementsByTagName("td");
+      // debugger;
+
+      for(var j=0; j<tds.length;j++){
+        if(tds[j].textContent.toUpperCase().match(word.toUpperCase())!=null){
+          rows[i].style.display=""; // reset to keep
+          break;
+        }
+        if(j==tds.length-1){ // last column, still no match
+          rows[i].style.display="none"; // remove this row
+        }
+      };
+            
+    }
+}
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementsByClassName("overview_table");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table[0].rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("td")[n];
+      y = rows[i + 1].getElementsByTagName("td")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerText.toLowerCase() > y.innerText.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerText.toLowerCase() < y.innerText.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
